d7c5edaa8c12603ab8cbf88ca4b1d7e2e10a2557
Rafael de Souza Freire <rafaeldesouzafreire@pop-os.localdomain>
2022-09-07 13:40:58 -0300

    Data select

--- backend/src/main/java/com/project/dsmeta/repositories/SalesRepository.java
+++ backend/src/main/java/com/project/dsmeta/repositories/SalesRepository.java
@@ -1,9 +1,17 @@
 package com.project.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.project.dsmeta.entities.Sales;
 
 public interface SalesRepository extends JpaRepository<Sales, Long> {
+
+	@Query("SELECT obj FROM Sales obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sales> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
--- backend/src/main/java/com/project/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/project/dsmeta/services/SalesService.java
@@ -1,8 +1,12 @@
 package com.project.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.project.dsmeta.entities.Sales;
@@ -14,8 +18,14 @@ public class SalesService {
 	@Autowired
 	SalesRepository salesRepository;
 	
-	public List<Sales> findSales() {
-		return salesRepository.findAll(); 
+	public Page<Sales> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return salesRepository.findSales(min, max, pageable); 
 	}
 
 }
